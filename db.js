3f5de5e26a1df3790e853106b8836385d70ce981
IvanAzamar <81318326+IvanAzamar@users.noreply.github.com>
2025-08-05 18:11:19 -0600

    Update db.js
    c4

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 [
   {
     "id": 1,
-    "name": "MacBook Pro",
+    "name": "MacBook Air",
     "image": "guitarra_01",
     "description": "Laptop de alto rendimiento ideal para diseño, programación y edición profesional.",
     "price": 1999
